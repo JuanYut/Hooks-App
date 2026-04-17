a326b7c401e6b7eb2eafb3dd575c5c955a84aac0
Juan Santillan <juanyut97@hotmail.com>
2026-04-16 23:07:48 -0600

    PokemonPage with Hook

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { PokemonPage } from "./03-example/PokemonPage";
 // import HooksApp from "./HooksApp";
 // import { TrafficLight } from "./01-useState/TrafficLight";
 // import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
-import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
+// import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <div>
       {/* <TrafficLight /> */}
       {/* <TrafficLightWithEffect /> */}
-      <TrafficLightWithHook />
+      {/* <TrafficLightWithHook /> */}
+      <PokemonPage />
     </div>
   </StrictMode>,
 );
